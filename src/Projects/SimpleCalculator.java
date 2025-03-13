73cf558be7a35aaa0d1d56fc93f8cddf4f089480
Lucas <lteixeira507@gmail.com>
2025-03-13 10:24:35 -0400

    Fix: logic in error option

--- src/Projects/SimpleCalculator.java
+++ src/Projects/SimpleCalculator.java
@@ -13,20 +13,26 @@ public class SimpleCalculator {
         System.out.println("3) Multiplicar");
         System.out.println("4) Dividir");
         System.out.println("5) Sair");
-        int opcao = sc.nextInt();
+        int opcao;
+        opcao = sc.nextInt();
 
-        while(opcao > 0 && opcao < 5){
-            System.out.println("Agora digite o primeiro valor: ");
-            double inputOne = sc.nextDouble();
-            System.out.println("Agora digite o segundo valor: ");
-            double inputTwo = sc.nextDouble();
+        while(opcao != 5){
+            if(opcao <= 0 || opcao > 5){
+                System.out.println("|------------------------------|");
+                System.out.println("Opcao Invalida, tente novamente.");
+                System.out.println("|------------------------------|");
 
-            if(opcao == 1) System.out.println("O valor da soma eh: "+SumNumber(inputOne, inputTwo));
-            if(opcao == 2) System.out.println("O valor da subtracao eh: "+MinusNumber(inputOne, inputTwo));
-            if(opcao == 3) System.out.println("O valor da multiplicacao eh: "+MultixNumber(inputOne, inputTwo));
-            if(opcao == 4) System.out.println("O valor da divisao eh: "+DivisonNumber(inputOne, inputTwo));
+            }else{
+                System.out.println("Agora digite o primeiro valor: ");
+                double inputOne = sc.nextDouble();
+                System.out.println("Agora digite o segundo valor: ");
+                double inputTwo = sc.nextDouble();
 
-
+                if(opcao == 1) System.out.println("O valor da soma eh: "+SumNumber(inputOne, inputTwo));
+                if(opcao == 2) System.out.println("O valor da subtracao eh: "+MinusNumber(inputOne, inputTwo));
+                if(opcao == 3) System.out.println("O valor da multiplicacao eh: "+MultixNumber(inputOne, inputTwo));
+                if(opcao == 4) System.out.println("O valor da divisao eh: "+DivisonNumber(inputOne, inputTwo));
+            }
             System.out.println("Escolha qual operacao abaixo:");
             System.out.println("1) Somar");
             System.out.println("2) Subtrair");
@@ -34,10 +40,9 @@ public class SimpleCalculator {
             System.out.println("4) Dividir");
             System.out.println("5) Sair");
             opcao = sc.nextInt();
+
         }
         sc.close();
-
-
     }
 
     public static double SumNumber(double numberOne, double numberTwo){
